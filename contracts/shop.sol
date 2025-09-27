e9a12f1a6bc777b41582b530f0bc595912e71d73
Tanmay Sharma <148971810+10m4y@users.noreply.github.com>
2025-09-27 23:43:50 +0530

    Update ChestNFT to use ERC721Enumerable and add metadata
    Refactor ChestNFT contract to use ERC721Enumerable and update minting logic to include metadata.

--- contracts/shop.sol
+++ contracts/shop.sol
@@ -1,28 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ChestNFT is ERC721, Ownable {
+contract ChestNFT is ERC721Enumerable, Ownable {
     uint256 public nextTokenId;
-    address public chestOpener;
 
-    constructor(address initialOwner) ERC721("Chest NFT", "CNFT") Ownable(initialOwner) {}
+    struct NFTMetadata {
+        string name;
+        string prize;
+        string image; // IPFS CID or full ipfs:// link
+    }
 
-    modifier onlyChestOpener() {
-        require(msg.sender == chestOpener, "Not ChestOpener");
+    mapping(uint256 => NFTMetadata) public tokenMetadata;
+    mapping(address => bool) public minters;
+
+    modifier onlyMinter() {
+        require(minters[msg.sender], "Not a minter");
         _;
     }
 
-    function setChestOpener(address _chestOpener) external onlyOwner {
-        chestOpener = _chestOpener;
+    constructor() ERC721("ChestNFT", "CNFT") Ownable(msg.sender) {}
+
+    function setMinter(address minter, bool allowed) external onlyOwner {
+        minters[minter] = allowed;
     }
 
-    function mint(address to) external onlyChestOpener returns (uint256) {
-        uint256 tokenId = nextTokenId++;
+    function mintWithMetadata(
+        address to,
+        string memory name,
+        string memory prize,
+        string memory image
+    ) external onlyMinter returns (uint256) {
+        uint256 tokenId = nextTokenId;
         _safeMint(to, tokenId);
+        tokenMetadata[tokenId] = NFTMetadata(name, prize, image);
+        nextTokenId++;
         return tokenId;
     }
-    
+
+    /// Fetch all NFTs owned by a user
+    function getUserNFTs(address user) external view returns (NFTMetadata[] memory) {
+        uint256 count = balanceOf(user);
+        NFTMetadata[] memory items = new NFTMetadata[](count);
+        for (uint256 i = 0; i < count; i++) {
+            uint256 tokenId = tokenOfOwnerByIndex(user, i);
+            items[i] = tokenMetadata[tokenId];
+        }
+        return items;
+    }
 }
